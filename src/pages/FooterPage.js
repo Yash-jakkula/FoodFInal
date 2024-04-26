c2fd278ffed4ccddb0663d9b5dcd1f8c8005af66
yashwanthJakkula <yashjksoft@gmail.com>
2024-04-26 12:58:10 +0530

    done

--- src/pages/FooterPage.js
+++ src/pages/FooterPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ExternalLink } from 'react-external-link';
-import './Footer.css'
 
 const FooterPage = () => {
     return(
