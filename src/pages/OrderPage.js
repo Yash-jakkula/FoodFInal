e1c2ea4f9dccc13487096e72880acb5e5279d364
yashwanthJakkula <yashjksoft@gmail.com>
2024-04-29 10:36:27 +0530

    changes made

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -336,6 +336,7 @@ let teaConcentrationsData=[
 ]
 let totalData=[...breakfastMixesData,...biryaniData,...teaConcentrationsData]
 function OrderPage() {
+  
   const sno=Number(localStorage.getItem("productId")) 
   const category=(localStorage.getItem("category"))
   const [formdata, setFormdata] = useState({
