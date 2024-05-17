527cb6d59ec2a1ed4503cc3588f5e531ac5cb2b4
yashwanthJakkula <yashjksoft@gmail.com>
2024-05-17 14:11:50 +0530

    changed

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -16,7 +16,7 @@ const Homepage = () => {
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="/s2.jpg" class="d-block w-100" alt="..." />
+      <img src="/S2.jpg" class="d-block w-100" alt="..." />
     </div>
     <div class="carousel-item">
       <img src="/chef.png" class="d-block w-100" alt="..." />
